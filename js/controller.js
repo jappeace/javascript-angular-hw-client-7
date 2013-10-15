45ccd3c00c7d840c4e29db7f28c7c6e1220a57d1
Joep Kemperman <kazooiej@gmail.com>
2013-10-15 13:48:13 +0200

    Table is correctly passed to reserveTable(table)

--- js/controller.js
+++ js/controller.js
@@ -52,6 +52,7 @@ function TableReserveController($scope, tableService) {
 	$scope.tables = tableService.getTables();
 
 	$scope.reserveTable = function(table) {
+		$scope.reserveMessage = "You have reserved table " + table.id + "!";
 		console.log(table);
 	};
 }
