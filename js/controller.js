e032a4e006413c25ea1566589cee39e0e5bb8d84
Chris Mulder <chris@chris-debian.thuis>
2013-10-15 12:07:54 +0200

    Updated controller for models

--- js/controller.js
+++ js/controller.js
@@ -1,34 +1,44 @@
-function RestaurantController($scope) {
-	$scope.tables = [
-		{
-			id: 1,
-			seats: 8,
-			reserved: false,
-			ordered: []
-		},
-		{
-			id: 2,
-			seats: 2,
-			reserved: false,
-			ordered: []
-		},
-		{
-			id: 3,
-			seats: 5,
-			reserved: false,
-			ordered: []
-		},
-		{
-			id: 4,
-			seats: 12,
-			reserved: false,
-			ordered: []
-		},
-		{
-			id: 5,
-			seats: 4,
-			reserved: false,
-			ordered: []
-		}
-	];
+angular.module('restaurant', [])
+	.service('tableService', function() {
+		var tables = [
+			{
+				id: 1,
+				seats: 8,
+				reserved: false,
+				ordered: []
+			},
+			{
+				id: 2,
+				seats: 2,
+				reserved: false,
+				ordered: []
+			},
+			{
+				id: 3,
+				seats: 5,
+				reserved: false,
+				ordered: []
+			},
+			{
+				id: 4,
+				seats: 12,
+				reserved: false,
+				ordered: []
+			},
+			{
+				id: 5,
+				seats: 4,
+				reserved: false,
+				ordered: []
+			}];
+
+		return {
+			getTables: function() {
+				return tables;
+			}
+		};
+	})
+
+function RestaurantOrderController($scope, tableService) {
+	$scope.tables = tableService.getTables();
 }
